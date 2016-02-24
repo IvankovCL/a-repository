f532d6c7931632c1c692a40956cee747145364f5
IvankovCL <ivankovfa@mail.ru>
2016-02-25 00:11:18 +0300

    Добавлен скрипт для обработки МайСтемом и таблица с метаданными

---
+++ mstm.py
@@ -0,0 +1,11 @@
+import os
+
+for d, dirs, files in os.walk('C:\\search\\corpora'):
+    for filename in files:
+        path = os.path.join(d,filename)
+        os.makedirs(d.replace('corpora', 'MyStemmed'), exist_ok = True)
+        os.system('C:\\search\\mystem.exe -icd ' + path + ' ' + path.replace('.txt', '_MyStemmed.txt').replace('corpora', 'MyStemmed'))
+        os.makedirs(d.replace('corpora', 'xml'), exist_ok = True)
+        os.system('C:\\search\\mystem.exe -icd ' + path + ' ' + path.replace('.txt', '_MyStemmed.xml').replace('corpora', 'xml'))
+
+
