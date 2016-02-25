acf7a5676e10b17dc9c0b76a4523d1a9669cc8f4
IvankovCL <ivankovfa@mail.ru>
2016-02-25 08:03:58 +0300

    Исправлена оплошность с –format xml

--- mstm.py
+++ mstm.py
@@ -6,6 +6,6 @@ for d, dirs, files in os.walk('C:\\search\\corpora'):
         os.makedirs(d.replace('corpora', 'MyStemmed'), exist_ok = True)
         os.system('C:\\search\\mystem.exe -icd ' + path + ' ' + path.replace('.txt', '_MyStemmed.txt').replace('corpora', 'MyStemmed'))
         os.makedirs(d.replace('corpora', 'xml'), exist_ok = True)
-        os.system('C:\\search\\mystem.exe -icd ' + path + ' ' + path.replace('.txt', '_MyStemmed.xml').replace('corpora', 'xml'))
+        os.system('C:\\search\\mystem.exe -icd --format xml ' + path + ' ' + path.replace('.txt', '_MyStemmed.xml').replace('corpora', 'xml'))
 
 
